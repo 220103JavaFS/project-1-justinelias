c53ddd2051cba8d44005aa48b63bb4fbf1abf694
Elias Calagiu <eliasc81@gmail.com>
2022-01-25 09:47:25 -0800

    reimbursementtypecontroller complete

--- src/main/java/com/revature/controllers/ReimbursementTypeController.java
+++ src/main/java/com/revature/controllers/ReimbursementTypeController.java
@@ -3,24 +3,25 @@ package com.revature.controllers;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
-public class ReimbursementStatusController implements Controller{
+public class ReimbursementTypeController implements Controller{
 
-    private ReimbursementStatusService reimbursementstatusService = new ReimbursementStatusService();
+    private ReimbursementTypeService reimbursementTypeService = new ReimbursementTypeService;
 
-    private Handler getStatusById = (ctx) ->{
+    private Handler getTypeByID = (ctx) -> {
         if(ctx.req.getSession(false)!=null){
             String idString = ctx.pathParam("id");
             int id = Integer.parseInt(idString);
-
-            ctx.json(reimbursementStatusService.getStatusById(id));
+            ctx.json(reimbursementTypeService.getTypeById(id));
             ctx.status(200);
         }else {
             ctx.status(401);
         }
     };
 
+
+
     @Override
     public void addRoutes(Javalin app) {
-        app.get("/status/{id}", getStatusById);
+        app.get("/type/{id}", getTypeByID);
     }
 }
