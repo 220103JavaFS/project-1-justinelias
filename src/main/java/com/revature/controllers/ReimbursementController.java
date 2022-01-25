7c122062912c9f91e87fbcf8c5839e1f27cba2d7
Elias Calagiu <eliasc81@gmail.com>
2022-01-25 09:28:22 -0800

    reimbursement controller complete

--- src/main/java/com/revature/controllers/ReimbursementController.java
+++ src/main/java/com/revature/controllers/ReimbursementController.java
@@ -4,6 +4,8 @@ import com.revature.models.Reimbursement;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
+import java.util.List;
+
 public class ReimbursementController implements Controller{
 
     ReimbursementService reimbursementService = new ReimbursementService;
@@ -21,10 +23,38 @@ public class ReimbursementController implements Controller{
         }
     };
 
+    private Handler getAllReimbs = (ctx) ->{
+        if(ctx.req.getSession(false)!=null){
+            List<Reimbursement> list = reimbursementService.getAllReimbs();
+
+            ctx.json(list);
+            ctx.status(200);
+
+
+        }else{
+            ctx.status(401);
+        }
+    };
+
+    private Handler updateReimb = (ctx) -> {
+        if (ctx.req.getSession(false)!=null){
+            Reimbursement reimbursement = ctx.bodyAsClass(Reimbursement.class);
+           if(reimbursementService.updateReimb(reimbursement)){
+               ctx.status(202);
+           }else{
+               ctx.status(400);
+           }
+        }else{
+            ctx.status(401);
+        }
+    };
+
 
 
     @Override
     public void addRoutes(Javalin app) {
         app.post("/reimb", newReimb);
+        app.get("/reimb", getAllReimbs);
+        app.put("/reimb", updateReimb);
     }
 }
