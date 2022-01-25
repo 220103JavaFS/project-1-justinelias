18765a48969506f0af90829eb36d78dffa30646f
Elias Calagiu <eliasc81@gmail.com>
2022-01-25 08:25:34 -0800

    wrote most of App class but need to add Static files and controller when those are created

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,27 @@
+package com.revature;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static Javalin app;
+
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        app = Javalin.create((config -> {
+            config.addStaticFiles("", Location.EXTERNAL);
+        }));
+        configure();
+        app.start();
+    }
+
+    private static void configure(Controller...controllers){
+        for(Controller c: controllers){
+            c.addRoutes(app);
+        }
+    }
+}
