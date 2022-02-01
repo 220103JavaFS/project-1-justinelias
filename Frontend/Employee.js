23b69009c5dd12dd89666fd8f56966d59c0a2509
Elias Calagiu <eliasc81@gmail.com>
2022-02-01 06:02:15 -0800

    employee and login logic nearly complete

--- Frontend/Employee.js
+++ Frontend/Employee.js
@@ -1,9 +1,16 @@
-const url = "http://localhost:8000";
-let addReimbBtn = document.getElementById("addReimButton");
+const url = "http://localhost:8000/";
+
+if (sessionStorage.getItem("userSession") == null ){
+    window.location.replace(url + "Login.html");
+  }
+
+
+let addReimbBtn = document.getElementById("addReimbButton");
 let reimbTable = document.getElementById("reimbRequestTable");
 let logoutBtn = document.getElementById("logoutBtn");
 
 logoutBtn.addEventListener("click", logoutFunc);
+addReimbBtn.addEventListener("click", addReimb);
 
 async function logoutFunc(){
     
@@ -18,8 +25,87 @@ async function logoutFunc(){
   
     if(response.status===200){
         sessionStorage.clear();
-        window.location.replace(url + "login.html");
+        window.location.replace(url + "Login.html");
     }else{
       console.log("Logout unsuccessful");
     }
 }
+
+async function addReimb(){
+//temp code just to get authorization in testing
+let user = {ersUsername:"ChristmasCarol", ersPassword:"PassSw0Rd555"}
+
+let response2 = await fetch(url+"login",
+    {
+        method:"POST",
+        body : JSON.stringify(user),
+        credentials: "include"
+    }
+);
+
+if(response2.status===200){
+    console.log("Login successful");
+}else{
+    console.log("Login unsuccessful. Returned status cose of: "+response2.status);
+}
+    let reimbTypeId = 0;
+
+    if (document.querySelector("#reimbType").value=="Lodging"){
+        reimbTypeId = 1;
+    } else if (document.querySelector("#reimbType").value=="Travel"){
+        reimbTypeId = 2;
+    } else if (document.querySelector("#reimbType").value=="Food"){
+        reimbTypeId = 3;
+    } else if (document.querySelector("#reimbType").value=="Other"){
+        reimbTypeId = 4;
+    } 
+
+    let status = {
+        amount: document.getElementById("reimbAmount").value,
+        description: document.getElementById("reimbDescription").value,
+        type: reimbTypeId
+    }
+
+    let response = await fetch(url + "reimb",{
+        method: "POST",
+        body: JSON.stringify(status),
+        credentials: "include"
+    })
+
+    if(response.status === 201){
+        getAllReimbs();
+    }else{
+        console.log("Unsuccessful" +response.status);
+    }
+}
+
+// View current employee's own requests
+
+async function getAllReimbs(){
+    let response = await fetch(url + "reimb", {
+        credentials: "include"
+    })
+
+    if(response.status === 200){
+        let reimbs = await response.json();
+        populateReimbs(reimbs);
+    } else{
+        console.log("Unsuccessful" + response.status);
+    }
+}
+
+function populateReimbs(reimbs){
+    reimbTable.innerHTML = "";
+    for(let reimb of reimbs){
+        let row = document.createElement("tr");
+        for(let data in reimb){
+            let reimbData = reimb[data]; 
+            // create new cell with reimbData
+            let td = document.createElement("td");
+            td.innerHTML = reimbData;
+            row.appendChild(td);
+        }
+        reimbTable.appendChild(row);
+    }
+}
+
--- Frontend/Login.js
+++ Frontend/Login.js
@@ -1,23 +1,26 @@
 let loginBtn = document.getElementById("loginBtn");
+let userName = document.getElementById("username");
+let passWord = document.getElementById("password");
 
-
-const userName = "AliceInWL"
-const passWord = "$2a$10$aQWQsRhNqAlIvG7M4lliouKpzJlZmW3y5Siwx762NCC5DgzfbtSES"
-const url = "http:localhost:8080"
+// const userName = "AliceInWL"
+// const passWord = "$2a$10$aQWQsRhNqAlIvG7M4lliouKpzJlZmW3y5Siwx762NCC5DgzfbtSES"
+const url = "http:localhost:8080/"
 
 
 loginBtn.addEventListener("click", loginFunc);
 
 
 async function loginFunc(){
+  console.log("button worked");
     let user = {
-      username: userName,
-      password: passWord
+      username: userName.value,
+      password: passWord.value
     }
   
     let response = await fetch(
       url+"login",
       {
+        mode: "no-cors",
         method : "POST",
         body : JSON.stringify(user),
         credentials: "include"
@@ -25,7 +28,16 @@ async function loginFunc(){
     );
   
     if(response.status===200){
-      loginBtn.innerText = ""; 
+      let userCredentials = await response.json();
+        sessionStorage.setItem("userSession", userCredentials);
+
+        if (userCredentials.userRoleId === 1) {
+          console.log(userCredentials);
+
+            window.location.replace(url + "Employee.html");
+        } else {
+            window.location.replace(url + "Manager.html");
+        }
     }else{
       console.log("Login unsuccessful. Returned status code of:"+response.status);
     }
